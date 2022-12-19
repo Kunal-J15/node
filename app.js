c0c80053b5e7dc97fd698c3a0cc104b53203a488
KJ <subodhsjagtap19@gmail.com>
2022-12-19 17:40:09 +0530

    form and data store in file

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req,res)=>{
+   
+if(req.url==="/"){
+    const data = fs.readFileSync("./msg.txt").toString().split("\n");
+    data.pop();
+    let insert = data.map((e)=>'<h3>'+e.split("=")[1]+'</h3>').reduce((str,e)=>str=e+str,"");
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write("<head><title>My First Page</title></head>");
-if(req.url==="/") res.write('<body><h1>Welcome Home</h1></body>');
-else if(req.url==="/about")     res.write('<body><h1>Welcome About Us page</h1></body>');
-else if(req.url==="/node")    res.write('<body><h1>welcome to nodeJS project</h1></body>');
-else     res.write('<body><h1>Invalid request</h1></body>');
+    res.write(`<body>${insert}<form action="/message" method="post"><input type="text" name="mes"><button>Submit</button></form></body>`);
+    return res.end()
+}
+if(req.url==="/message" && req.method == "POST"){
+    const data=[];
+    req.on("data",(c)=>data.push(c))
+    req.on("end",(c)=>{
+        myMsg = Buffer.concat(data).toString();
+        console.log(myMsg);
+        fs.appendFile("msg.txt",myMsg+"\n",function (err) {
+            if(err) throw err;
+            console.log("saved");
+        });
+    })
+    res.statusCode= 302;
+    res.setHeader('Location',"/");
+    return res.end();
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write("<head><title>My First Page</title></head>");
+    res.write('<body><h1>Invalid request</h1></body>');
     res.write('</html>');
     res.end();
 });
