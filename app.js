f82bc54f2e1cc09364588d4fbd22b235d4435765
KJ <subodhsjagtap19@gmail.com>
2022-12-21 14:18:21 +0530

    express basic

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const route = require("./routes");
-const http = require("http");
-const server = http.createServer(route);
-server.listen(3000);
+const express = require("express");
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log("first middleware");
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("second middleware");
+    res.send({abc:1})
+});
+
+app.listen(3000);
+
+// const http = require("http");
+// const { createRequire } = require("module");
+// const server = http.createServer(route);
+// server.listen(3000);
